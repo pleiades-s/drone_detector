ff504410f1053ca3d1f839738e58dd3578e9bc18
Yun <58709201+pleiades-s@users.noreply.github.com>
2019-12-17 16:14:22 -0500

    Update inference.py

--- drone_detector/code/inference.py
+++ drone_detector/code/inference.py
@@ -1,3 +1,4 @@
+import sys
 import torch
 import torch.nn as nn
 from torchvision import datasets, transforms
@@ -8,50 +9,69 @@ import preprocessing
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
-
 #Config-------------------#
-data_length_list = ['0.1']
 test_batch = 16
-model_path = 'Conv_Lstm_mfcc_1_2019-11-21 03:47:46.pth' 
-model_name = 'Conv_Lstm_mfcc_1'
 #-------------------------#
-#TODO: get model info by using argument
+data_type = 'MFCC_1D'
 
-data_type = ''
+# argv check
+if(len(sys.argv) != 2):
+    print("WRONG ARGUMENT VECTOR")
+    exit()
 
-for data_length in data_length_list:
+model_dir_path = sys.argv[1] 
 
-    csv_path = '/home/stealthdrone/Desktop/data/csv/test_' + data_length + '.csv' 
-    file_path = '/home/stealthdrone/Desktop/data/trimmed/test/' + data_length
+#split by '/'
+model_path = model_dir_path.split('/')[-1]
+print("model_path", model_path)
+# model_path = 'Conv_Lstm_mfcc_1_2019-11-19_21_03:47:46.pth'
 
-    if model_name != 'resnet50':
-        model_class = preprocessing.class_for_name('network', model_name)
-        model = model_class()
-        data_type = 'MFCC_1D'
+model_name = model_path.split("_20")[0]
+# model_name = 'Conv_Lstm_mfcc_1'
+
+data_length = str(float(model_name.split('mfcc_')[1]) / 10)
+# data_length_list = ['0.1']
+
+print("model_dir_path: {}\nmodel_path: {}\nmodel_name: {}\ndata_length: {}"
+.format(model_dir_path, model_path, model_name, data_length))
+
+
+
+
+# for data_length in data_length_list:
+
+csv_path = '/home/stealthdrone/Desktop/data/csv/test_' + data_length + '.csv' 
+file_path = '/home/stealthdrone/Desktop/data/trimmed/test/' + data_length
+
     
-    else:
-        pretrained_model = preprocessing.class_for_name('torchvision.models', model_name)
-        model = pretrained_model(pretrained=True)
-        num_ftrs = model.fc.in_features
-        model.fc = nn.Linear(num_ftrs, 6)
-        data_type = 'MFCC_2D'
+if model_name != 'resnet50':
+    model_class = preprocessing.class_for_name('network', model_name)
+    model = model_class()
+    print(data_type)
     
-    dataset_class = preprocessing.class_for_name('test_dronedataset', 'TEST_'+ data_type)
+else:
+    pretrained_model = preprocessing.class_for_name('torchvision.models', model_name)
+    model = pretrained_model(pretrained=True)
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 6)
+    data_type = 'MFCC_2D'
+    print(data_type)
     
-    model.load_state_dict(torch.load('/home/stealthdrone/Desktop/model/' + model_path))
-    model.to(device)
-    model.eval()
+dataset_class = preprocessing.class_for_name('test_dronedataset', 'TEST_'+ data_type)
+    
+model.load_state_dict(torch.load('/home/stealthdrone/Desktop/modeltopi/' + model_path))
+model.to(device)
+model.eval()
 
-    test_set = dataset_class(csv_path, file_path)
-    kwargs = {'num_workers': 1, 'pin_memory': True} if device == 'cuda' else {} #needed for using datasets on gpu
+test_set = dataset_class(csv_path, file_path)
+kwargs = {'num_workers': 1, 'pin_memory': True} if device == 'cuda' else {} #needed for using datasets on gpu
 
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=test_batch, shuffle=False, **kwargs)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=test_batch, shuffle=False, **kwargs)
 
-    f = open("/home/stealthdrone/Desktop/output_log/inference_"+ model_name + '_' 
+f = open("/home/stealthdrone/Desktop/output_log/inference_"+ model_name + '_' 
     + data_length + "_output.txt", "w")
-    print_test.inference_print(model, test_loader, f)
+print_test.inference_print(model, test_loader, f)
 
-    f.close()
+f.close()
 
-    print("END")
-
+print("END")

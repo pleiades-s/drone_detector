d06c1ce724d8d84c2d947badc9aa6d30f52c9583
Yun <58709201+pleiades-s@users.noreply.github.com>
2019-12-16 19:57:18 -0500

    Update inference.py
    data_type 수정

--- drone_detector/code/inference.py
+++ drone_detector/code/inference.py
@@ -11,28 +11,29 @@ print(device)
 #Config-------------------#
 data_length_list = ['0.1']
 test_batch = 16
-model_path = 'Conv_Lstm_mfcc_1_2019-11-21 03:47:46.pth'
+model_path = 'Conv_Lstm_mfcc_1_2019-11-21 03:47:46.pth' 
 model_name = 'Conv_Lstm_mfcc_1'
-data_type = 'MFCC_1D'
 #-------------------------#
-
+data_type = ''
 
 for data_length in data_length_list:
 
     csv_path = '/home/stealthdrone/Desktop/data/csv/test_' + data_length + '.csv' 
     file_path = '/home/stealthdrone/Desktop/data/trimmed/test/' + data_length
 
-    dataset_class = preprocessing.class_for_name('test_dronedataset', 'TEST_'+ data_type)
-    
     if model_name != 'resnet50':
         model_class = preprocessing.class_for_name('network', model_name)
         model = model_class()
+        data_type = 'MFCC_1D'
     
     else:
         pretrained_model = preprocessing.class_for_name('torchvision.models', model_name)
         model = pretrained_model(pretrained=True)
         num_ftrs = model.fc.in_features
         model.fc = nn.Linear(num_ftrs, 6)
+        data_type = 'MFCC_2D'
+    
+    dataset_class = preprocessing.class_for_name('test_dronedataset', 'TEST_'+ data_type)
     
     model.load_state_dict(torch.load('/home/stealthdrone/Desktop/model/' + model_path))
     model.to(device)
